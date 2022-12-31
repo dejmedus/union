d7cd0f0f2e8132d07aa9df22bb2ddc3835a4cce1
dejmedus <email96123@gmail.com>
2022-12-30 22:43:55 -0700

    update styling

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -1,9 +1,12 @@
 const Banner = () => {
   return (
-    <div className="relative bg-neutral-800 px-4 py-3 pr-14 text-white">
-      <p className="text-left text-sm font-medium sm:text-center">
+    <div className="relative bg-zinc-800 px-4 py-3 pr-14">
+      <p className="text-left text-sm font-medium sm:text-center text-zinc-400">
         This is a demo build.{" "}
-        <a className="underline hover:translate-x-12" href="/docusaurus-link">
+        <a
+          className="underline hover:translate-x-12 text-zinc-400 hover:text-zinc-300"
+          href="/docusaurus-link"
+        >
           View the Design Guide &rarr;{" "}
         </a>
       </p>
--- components/Features.tsx
+++ components/Features.tsx
@@ -8,7 +8,9 @@ const Features = () => {
     >
       <div className="mx-auto max-w-screen-xl px-4 py-16 sm:px-6 lg:px-8">
         <div className="mx-auto max-w-lg text-center">
-          <h2 className="text-2xl font-bold sm:text-3xl">Know Thyself</h2>
+          <h2 className="text-2xl font-bold sm:text-3xl text-zinc-100">
+            Know Thyself
+          </h2>
 
           <p className="mt-4 text-zinc-300">
             Lorem ipsum, dolor sit amet consectetur adipisicing elit.
@@ -17,8 +19,8 @@ const Features = () => {
           </p>
         </div>
 
-        {/* <div className="mt-8 grid grid-cols-1 gap-8 md:grid-cols-2 lg:grid-cols-3">
-          <FeatureCard
+        {/* <div className="mt-8 grid grid-cols-1 gap-1 sm:grid-cols-2 md:grid-cols-4"> */}
+        {/* <FeatureCard
             header="Wow"
             body="Lorem ipsum dolor sit amet consectetur adipisicing elit."
           />
@@ -30,7 +32,11 @@ const Features = () => {
             header="Wow"
             body="Lorem ipsum dolor sit amet consectetur adipisicing elit. "
           />
-        </div> */}
+          <FeatureCard
+            header="Wow"
+            body="Lorem ipsum dolor sit amet consectetur adipisicing elit. "
+          /> */}
+        {/* </div> */}
 
         <div className="mt-12 text-center">
           <Button content="Get Started" path="/signup" />
@@ -50,16 +56,18 @@ interface FeatureCardProps {
 function FeatureCard({ header, body }: FeatureCardProps) {
   return (
     <div className="group block h-56">
-      <div className="relative flex h-full items-end rounded-3xl border-4 bg-zinc-50 border-violet-400 text-black p-8 transition group-hover:-translate-x-2 group-hover:-translate-y-2 group-hover:shadow-[8px_8px_0_0_#a78bfa]">
+      <div className="relative flex h-full items-end rounded-lg border-4 bg-white-20 border-zinc-800 p-8 transition group-hover:-translate-x-2 group-hover:-translate-y-2 group-hover:shadow-[8px_8px_0_0_#3b82f6]">
         <div className="lg:group-hover:absolute lg:group-hover:opacity-0">
-          <span className="text-3xl sm:text-4xl" role="img" aria-hidden="true">
-            📆
+          <span className="text-3xl sm:text-6xl" role="img" aria-hidden="true">
+            ✨
           </span>
-          <p className="mt-4 text-xl font-bold sm:text-2xl">{header}</p>
+          <p className="mt-4 text-xl font-bold sm:text-3xl text-zinc-900">
+            {header}
+          </p>
         </div>
 
         <div className="absolute opacity-0 lg:group-hover:relative lg:group-hover:opacity-100">
-          <h3 className="text-2xl font-bold">{header}</h3>
+          <h3 className="text-2xl font-bold text-zinc-900">{header}</h3>
 
           <p className="mt-4 text-lg font-medium leading-relaxed text-black">
             {body}
