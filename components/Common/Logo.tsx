48b1dd57ae2e50634f0439574203124279bf69cc
dejmedus <email96123@gmail.com>
2022-12-31 15:18:44 -0700

    update logo

--- components/Common/Logo.tsx
+++ components/Common/Logo.tsx
@@ -2,9 +2,13 @@ import Link from "next/link";
 
 const Logo = () => {
   return (
-    <Link className="block text-stone-600" href="/">
-      <h2 className="text-blue-400 hover:text-blue-500 font-bold border-full text-xl">
-        Union
+    <Link className="block text-stone-600 group" href="/">
+      <h2 className="text-blue-400 font-bold border-full text-xl group-hover:text-blue-500">
+        <span className="inline-flex h-7 w-7 items-center justify-center rounded-lg bg-blue-100 group-hover:bg-blue-200">
+          <span className="sr-only">Logo</span>
+          🪢
+        </span>
+        {"  "}Union
       </h2>
     </Link>
   );
