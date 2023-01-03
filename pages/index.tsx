3a12e2939a54a2be9caaa5aa6ba4820522ac8955
dejmedus <email96123@gmail.com>
2023-01-02 17:36:42 -0700

    update prisma schema & add mock data

--- pages/index.tsx
+++ pages/index.tsx
@@ -15,6 +15,55 @@ import { useSession } from "next-auth/react";
 
 export default function Home() {
   const { data: session, status } = useSession();
+  // mock user details
+  const user = {
+    name: "Bob",
+    notifications: [{ id: 1, read: true, message: "You have a new match!" }],
+    gender_pref: ["S", "T", "H", "O"],
+    matches: [
+      {
+        id: 1,
+        name: "Sam",
+        gender: "T",
+        values: ["honesty", "kindness"],
+        badges: [],
+        photo: null,
+        intro:
+          "I found a rock in a Waltmart parking lot yesterday shaped like a poptart",
+        match_level: "LEVEL_1",
+      },
+      {
+        id: 2,
+        name: "Claire",
+        gender: "S",
+        values: ["honesty"],
+        badges: ["heart", "star", "dialog"],
+        photo: null,
+        intro: "Seeking a step-dad for my pomeranian Mikey Bobikey",
+        match_level: "LEVEL_1",
+      },
+      {
+        id: 3,
+        name: "Terry",
+        gender: "O",
+        values: ["honesty", "kindness", "acceptance"],
+        badges: ["dialog"],
+        photo: null,
+        intro: "I dunno what to write here",
+        match_level: "LEVEL_2",
+      },
+      {
+        id: 4,
+        name: "Craig",
+        gender: "H",
+        values: ["honesty", "kindness", "acceptance", "loyalty"],
+        badges: ["heart"],
+        photo: null,
+        intro: "Beer. Fishing. Trucks. Hallmark Movies.",
+        match_level: "LEVEL_1",
+      },
+    ],
+  };
   return (
     <>
       <Head>
@@ -25,9 +74,8 @@ export default function Home() {
       </Head>
       <main>
         <Banner />
-        <Navbar session={session} />
-
-        {status == "unauthenticated" ? (
+        <Navbar session={session} user={user} />
+        {status == "loading" ? null : status == "unauthenticated" ? (
           <>
             <Hero />
             <Features />
@@ -35,7 +83,7 @@ export default function Home() {
             <FAQs />
           </>
         ) : (
-          <Match session={session} />
+          <Match session={session} user={user} />
         )}
         <Footer />
       </main>
